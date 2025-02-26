6de0d520582cd5a53fc0f9fcbba519bb831e05fd
kuromaaaaa <kuromaxa@gmail.com>
2025-02-26 11:01:09 +0900

    [update,add] FartManagerのFartGaugeにゲッターを追加 インターフェースの関数を呼ぶだけのGimmickSpikeを追加

--- Assets/Kuroda/FartGauge/FartManager.cs
+++ Assets/Kuroda/FartGauge/FartManager.cs
@@ -5,6 +5,8 @@ public class FartManager : MonoBehaviour
 {
     [SerializeField] private float fartParSecond;
     FartGauge _fartGauge = new FartGauge();
+    public FartGauge FartGauge
+    { get => _fartGauge; }
 
     private void Start()
     {
