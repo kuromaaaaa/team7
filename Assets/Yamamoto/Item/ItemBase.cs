d63cd745aeb6f71a1c55e25f0bec7df9e0485700
YamamotoU518 <hw37hnmr@gmail.com>
2025-02-26 21:19:19 +0900

    [Add] アイテム（きのこ）の追加

--- Assets/Yamamoto/Item/ItemBase.cs
+++ Assets/Yamamoto/Item/ItemBase.cs
@@ -2,12 +2,13 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-public class ItemBase : MonoBehaviour
+public class ItemBase : MonoBehaviour, PauseManager.IPauseable
 {
     [SerializeField, Header("RespawnItem")] private UpdateItem updateItem;
     [SerializeField, Header("アイテム効果時間")] protected int _length;
 
     private CircleCollider2D _collider;
+    private bool _isPaused;
 
     private void Start()
     {
@@ -19,7 +20,17 @@ public class ItemBase : MonoBehaviour
     private async UniTask Destroy(Collider2D other)
     {
         var tokenSource = new CancellationTokenSource();
-        await UniTask.Delay( _length * 1000, cancellationToken: tokenSource.Token);
+        float elapsed = 0f;
+        float duration = _length;
+        while (elapsed < duration)
+        {
+            if (!_isPaused) // ポーズ中でない場合のみ時間を進める
+            {
+                elapsed += Time.deltaTime;
+            }
+            await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken: tokenSource.Token);
+        }
+        // await UniTask.Delay( _length * 1000, cancellationToken: tokenSource.Token);
         EndAction(other);
         tokenSource.Cancel();
         tokenSource.Dispose();
@@ -32,11 +43,21 @@ public class ItemBase : MonoBehaviour
         if (!other.CompareTag("Player")) return;
         
         // todo: sound　アイテムゲット
-        // AudioManager.SE.Play(AudioClipType.SE_Player_ItemGet);
+        AudioManager.SE.Play(AudioClipType.SE_Player_ItemGet);
         Action(other);
         Destroy(other).Forget();
         updateItem.EnableItem();
         updateItem.RespawnItem().Forget();
         _collider.enabled = false;
+    }
+
+    public void Pause()
+    {
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        _isPaused = false;
     }
 }
