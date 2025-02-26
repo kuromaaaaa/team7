02e2e2e088834c3e364e900f4ab96f8229958d9c
Shigema <tsubakyun4869@gmail.com>
2025-02-26 12:42:24 +0900

    [add] 無重力用のフラグ

--- Assets/Shigema/Player/Move.cs
+++ Assets/Shigema/Player/Move.cs
@@ -4,12 +4,18 @@ public class Move : MonoBehaviour
 {
     [SerializeField, Header("強さ")] private float _power = 5f;
     [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
+
+    [Range(0, 1)] [SerializeField, Header("ガスの消費量")]
+    private float _gasConsumption = 0.2f;
+
     private Rigidbody2D _rigidbody2D;
     private bool _canJump = true;
 
     private float _defaultPower;
+
     //todo: 真になる条件）通常：地面設置時／無重力：一定時間内継続
     // todo: ゲージ消費機能
+    private bool _isZeroGravity;
 
     private void Start()
     {
@@ -22,11 +28,16 @@ public class Move : MonoBehaviour
         Jump();
     }
 
+    private void Judge()
+    {
+        // ゲージの残量があればジャンプ可能
+    }
+
     private void Jump()
     {
         if (Input.GetKeyDown(_jumpKey) && _canJump)
         {
-            _canJump = false;
+            if (!_isZeroGravity) _canJump = false;
             Vector2 velocity = _rigidbody2D.linearVelocity;
             velocity += (Vector2)transform.up * _power;
             _rigidbody2D.linearVelocity = velocity;
@@ -45,5 +56,11 @@ public class Move : MonoBehaviour
     {
         _power = power;
         return _defaultPower;
+    }
+
+    public void ChangeFlag(bool flag)
+    {
+        _isZeroGravity = flag;
+        _canJump = flag;
     }
 }
